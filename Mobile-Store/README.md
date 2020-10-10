0744ac1cad58a936fa5650b49cc06d9f6589f804
abhishek-dubal-au6 <56307961+abhishek-dubal-au6@users.noreply.github.com>
2020-10-09 21:50:04 -0700

    final

---
+++ Mobile-Store/README.md
@@ -0,0 +1,17 @@
+Capstone Project
+
+# MERN Stack - The Mobile Store
+> The Mobile Store website built with the MERN stack with React Context API for state management, pure CSS for style
+
+
+ Run the client & server with concurrently
+ `npm run dev`
+
+Run the Express server only
+`npm run server`
+
+Run the React client only
+`npm run client`
+
+
+
--- Mobile-Store/server.js
+++ Mobile-Store/server.js
@@ -5,15 +5,18 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 
 const app = express()
 app.use(express.json())
+app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors())
 app.use(fileUpload({
     useTempFiles: true
 }))
+
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
